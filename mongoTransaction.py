b5e09e138eea78b08faff4e5d9b5e43c310db74e
RexMudanya <musungumudanya@gmail.com>
2019-03-19 14:19:27 +0300

    added Transaction manage

---
+++ mongoTransaction.py
@@ -0,0 +1,14 @@
+from pymongo import MongoClient
+import datetime
+import pprint
+import copy
+
+client = MongoClient('localhost', 27017)
+db = client["FoodApp"]
+collection = db["Transaction"]
+posts = collection
+
+#new transaction (inhouse)
+
+#new transaction (inhouse)
+
