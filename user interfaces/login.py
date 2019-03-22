8340fbffbecf5a0a30a91be56f60a3eabb926f70
RexMudanya <musungumudanya@gmail.com>
2019-03-22 10:07:34 +0300

    added main window

--- user interfaces/login.py
+++ user interfaces/login.py
@@ -7,23 +7,23 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
 class Window(QMainWindow):
-	"""docstring for Window"""
-	def __init__(self):
-		super().__init__()
-		
-		self.title = "Client Login"
-		self.top = 400
-		self.left= 200
-		self.width= 500
-		self.height=500
-		#self.setWindowIcon(QtGui.QIcon("icon.jpg"))
+    """docstring for Window"""
+    def __init__(self):
+        super().__init__()
+        
+        self.title = "Client Login"
+        self.top = 400
+        self.left= 200
+        self.width= 500
+        self.height=500
+        #self.setWindowIcon(QtGui.QIcon("icon.jpg"))
 
-		self.InitUI()
+        self.InitUI()
 
-	def InitUI(self):
-		self.setWindowTitle(self.title)
-		self.setGeometry(self.top, self.left, self.width, self.height)
-		self.show()
+    def InitUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.top, self.left, self.width, self.height)
+        self.show()
 
 App = QApplication(sys.argv)
 window = Window()
