2c2d3902ca6838dbfe073245f271a47db8478972
RexMudanya <musungumudanya@gmail.com>
2019-03-23 11:43:49 +0300

    changes in ui

--- user interfaces/login.py
+++ user interfaces/login.py
@@ -3,10 +3,10 @@ window for client login to system
 email + password login
 '''
 from PyQt5 import QtGui
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton
 import sys
 
-class Window(QMainWindow):
+class LoginWindow(QMainWindow):
     """docstring for Window"""
     def __init__(self):
         super().__init__()
@@ -18,6 +18,16 @@ class Window(QMainWindow):
         self.height=500
         #self.setWindowIcon(QtGui.QIcon("icon.jpg"))
 
+        name_label =  QLabel("Username: ", self)
+        #name_label.move(200,200)       
+        name_label.setGeometry(80,50,101,31)
+
+        pwd_label = QLabel("Password: ", self)
+        pwd_label.setGeometry(80,110,101,31)
+
+        login_btn = QPushButton("Login", self)
+        login_btn.setGeometry(300, 210, 89, 25)
+
         self.InitUI()
 
     def InitUI(self):
@@ -26,5 +36,5 @@ class Window(QMainWindow):
         self.show()
 
 App = QApplication(sys.argv)
-window = Window()
+window = LoginWindow()
 sys.exit(App.exec())
